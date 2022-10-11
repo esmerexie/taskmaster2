ec9e480b0ea2b9e99d320477f9e8454b9007cbca
esmerexie <esmeraldarexie@gmail.com>
2022-10-10 22:15:40 -0700

    fixed issue on app crashing upon clicking add task button

--- app/src/main/java/com/example/taskmaster/activites/SettingsPageActivity.java
+++ app/src/main/java/com/example/taskmaster/activites/SettingsPageActivity.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.example.taskmaster.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsPageActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
 
     public static final String USER_NAME_TAG = "userName";
+
+    List<String> teamNames = null;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +41,10 @@ public class SettingsPageActivity extends AppCompatActivity {
             userNameEdited.setText(userName);
         }
 
+        teamNames = new ArrayList<>();
         setUpSubmitButton();
     }
+
 
     private void setUpSubmitButton(){
 
